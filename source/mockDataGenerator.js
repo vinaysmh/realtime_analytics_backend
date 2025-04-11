88bcb3f4be1152763674d43bfc38f9353e07ff9b
vinay saurabh <vinaysmh@gmail.com>
2025-04-11 20:55:11 +0530

    Updated mock data generation
    must never return any random 0 value
    duration to be double always to prevent client side parsing and implementing strong typing

--- source/mockDataGenerator.js
+++ source/mockDataGenerator.js
@@ -1,11 +1,10 @@
 function generateMockData() {
-    return {
-      timestamp: new Date().toISOString(),
-      active_users: Math.floor(Math.random() * 100),
-      page_views: Math.floor(Math.random() * 200),
-      avg_session_duration: (Math.random() * 5 + 1).toFixed(2),
-    };
-  }
-  
-  module.exports = generateMockData;
-  
+  return {
+    timestamp: new Date().toISOString(),
+    page_views: Math.floor(Math.random() * 200) + 1,
+    active_users: Math.floor(Math.random() * 100) + 1,
+    avg_session_duration: parseFloat((Math.random() * 5 + 1).toFixed(2)),
+  };
+}
+
+module.exports = generateMockData;
